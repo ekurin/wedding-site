3fede8ce61dcd1b050f75689f75499725a2c5c71
Erik Kurin <ekurin@gmail.com>
2015-12-25 17:42:01 +0100

    Post form result to Google Forms
    Posts form to Google Form via Ajax. This is kind of a workaround for not using Googls embedded form. Google don't return status 200 when post is complete but rather status 0.
    
    jqBootstrapValidator which is included in AgencyTheme for form validation doesn't work with Bootstrap 3

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,8 @@
 $(function() {
 
+    /* jqBootstrapValidator don't work with bootstrap 3
+       since control-group is renamed to form-group in BS 3
+    */
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -9,38 +12,55 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
+            var namePartner = $("input#name-partner").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var allergies = $("input#allergies").val();
+            var allergiesPartner = $("input#allergies-partner").val();
             var message = $("textarea#message").val();
+            var fridayAttend = $("input#attending-friday").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://docs.google.com/forms/d/1sV6FhRoBHfmvg4wZrP4qzgoZOa18ualYlYGZ0oMiX8A/formResponse",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    "entry.400846965": name,
+                    "entry.408869072": namePartner,
+                    "entry.1283878840": email,
+                    "entry.182502062": phone,
+                    "entry.1305681393": allergies,
+                    "entry.1054903899": allergiesPartner,
+                    "entry.1286098234": message,
+                    "entry.296385398": fridayAttend
                 },
+                dataType: "xml",
                 cache: false,
-                success: function() {
+                statusCode: {
+                    // Google Forms return status 0 when POST completes
+                    0: function() {
+                        sucessHandler();
+                    }
+                },
+                success: sucessHandler = function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tack för din anmälan " + firstName + " </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(xhr, textStatus, errorThrown) {
+                    // Google Forms return status 0 when POST completes
+                    if (xhr.status == 0) { return;}
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
