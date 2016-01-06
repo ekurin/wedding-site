f573c6a783c5d392f1bbe18f08f403b3a257066b
Erik Kurin <ekurin@gmail.com>
2016-01-06 13:09:56 +0100

    Removed title on landingpage

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -27,10 +27,12 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
+			$('a.navbar-brand').show()
 			classie.add( header, 'navbar-shrink' );
 		}
 		else {
 			classie.remove( header, 'navbar-shrink' );
+			$('a.navbar-brand').hide()
 		}
 		didScroll = false;
 	}
